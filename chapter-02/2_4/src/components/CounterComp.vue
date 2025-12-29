d0f3bd07f81c076b1537966381b4026f4a6f0bc4
ibiscum <ulf.luecking@gmail.com>
2025-12-29 20:55:50 +0100

    ch02

---
+++ chapter-02/2_4/src/components/CounterComp.vue
@@ -0,0 +1,30 @@
+<template>
+  <button @click="increase">
+    {{ count }}
+  </button>
+  <button @click="decrease">
+    {{ count }}
+  </button>
+</template>
+
+<script setup lang="ts">
+  import { ref } from 'vue'
+  const count = ref(0)
+
+  function formattedNumber() {
+    console.log(count.value) // 0
+  }
+
+  function increase() {
+    count.value++;
+    console.log(count.value)
+  }
+
+  function decrease() {
+    count.value--;
+    console.log(count.value)
+  }
+
+formattedNumber();
+
+</script>
