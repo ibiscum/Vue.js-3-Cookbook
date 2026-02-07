0fea7f7dd3ca00550c62173ba0386b7106651af6
ibiscum <ulf.luecking@gmail.com>
2026-02-07 17:52:02 +0100

    2_3

--- chapter-02/2_3/animal.js
+++ chapter-02/2_3/animal.js
@@ -1,27 +1,28 @@
-export var FoodChainType;
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Animal = exports.FoodChainType = void 0;
+var FoodChainType;
 (function (FoodChainType) {
     FoodChainType["Carnivorous"] = "carnivorous";
     FoodChainType["Herbivorous"] = "herbivorous";
     FoodChainType["Omnivorous"] = "omnivorous";
-})(FoodChainType || (FoodChainType = {}));
-export class Animal {
-    name;
-    sound;
-    family;
-    foodChainType;
-    constructor(params) {
+})(FoodChainType || (exports.FoodChainType = FoodChainType = {}));
+var Animal = /** @class */ (function () {
+    function Animal(params) {
         this.name = params.name;
         this.sound = params.sound || '';
         this.family = params.family;
         this.foodChainType = params.foodChainType;
     }
-    whoAmI() {
-        console.log(`I am a ${this.name}, my family is ${this.family}. My diet is ${this.foodChainType}.`);
+    Animal.prototype.whoAmI = function () {
+        console.log("I am a ".concat(this.name, ", my family is ").concat(this.family, ". My diet is ").concat(this.foodChainType, "."));
         //   if (this.sound) {
         //     console.log([...Array(2).fill(this.sound)].join(', '));
         //   }
-    }
-    makeSound() {
+    };
+    Animal.prototype.makeSound = function () {
         console.log(this.sound);
-    }
-}
+    };
+    return Animal;
+}());
+exports.Animal = Animal;
--- chapter-02/2_3/index.js
+++ chapter-02/2_3/index.js
@@ -1,18 +1,37 @@
-import { Animal, FoodChainType } from "./animal.js";
-console.log(FoodChainType.Carnivorous);
-console.log(FoodChainType.Herbivorous);
-console.log(FoodChainType.Omnivorous);
-class Dog extends Animal {
-    constructor() {
-        super({
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+var animal_js_1 = require("./animal.js");
+console.log(animal_js_1.FoodChainType.Carnivorous);
+console.log(animal_js_1.FoodChainType.Herbivorous);
+console.log(animal_js_1.FoodChainType.Omnivorous);
+var Dog = /** @class */ (function (_super) {
+    __extends(Dog, _super);
+    function Dog() {
+        return _super.call(this, {
             name: 'Dog',
             sound: 'Wof!',
             family: 'Canidae',
-            foodChainType: FoodChainType.Carnivorous,
-        });
+            foodChainType: animal_js_1.FoodChainType.Carnivorous,
+        }) || this;
     }
-}
-let dog = new Dog();
+    return Dog;
+}(animal_js_1.Animal));
+var dog = new Dog();
 console.log(dog);
 console.log(dog.whoAmI());
 console.log(dog.makeSound());
